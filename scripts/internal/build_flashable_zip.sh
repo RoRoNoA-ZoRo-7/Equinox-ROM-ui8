205dcb053d9440580142f13546528ccf7520c99c
ihaten69 <youknowmexd69@gmail.com>
2025-09-07 08:15:18 +0530

    Update build_flashable_zip.sh

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -558,7 +558,7 @@ GENERATE_BUILD_INFO()
     true
 }
 
-ZIP_FILE_NAME="ExtremeROM_${ROM_CODENAME}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}.zip"
+ZIP_FILE_NAME="${ROM_CODENAME}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}.zip"
 # ]
 
 echo "Set up tmp dir"
