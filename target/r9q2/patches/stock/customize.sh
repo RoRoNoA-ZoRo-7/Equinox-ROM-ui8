9738418596d280067ef7e196108a6ba183aa58fe
ihaten69 <youknowmexd69@gmail.com>
2025-09-09 06:33:47 +0530

    Update customize.sh

--- target/r9q2/patches/stock/customize.sh
+++ target/r9q2/patches/stock/customize.sh
@@ -1,9 +1,3 @@
-echo "- Fixing Google Assistant"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4HEXAGON"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4HEXAGON"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx3HEXAGON" 0 0 755 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx3HEXAGON" 0 0 755 "u:object_r:system_file:s0"
-
 
 echo "- Adding stock system features"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/com.sec.feature.cover.clearsideviewcover.xml" 0 0 644 "u:object_r:system_file:s0"
