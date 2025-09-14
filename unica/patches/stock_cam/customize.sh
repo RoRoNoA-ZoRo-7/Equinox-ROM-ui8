b57c380ad71505815596f21c7641963bde4a63ff
ihaten69 <youknowmexd69@gmail.com>
2025-09-14 09:01:47 +0530

    Add files via upload

---
+++ unica/patches/stock_cam/customize.sh
@@ -0,0 +1,11 @@
+DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/oat"
+DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
+ADD_TO_WORK_DIR "r9qxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
+
+r12sxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
+    else
+        echo "TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
+    fi
+else
+    echo "SOURCE_HAS_MASS_CAMERA_APP is set. Ignoring"
+fi
