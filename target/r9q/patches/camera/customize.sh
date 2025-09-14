5a2b4f02abca4d09942b45cbaef9379f18938ec4
ihaten69 <youknowmexd69@gmail.com>
2025-09-14 17:21:54 +0530

    Update customize.sh

--- target/r9q/patches/camera/customize.sh
+++ target/r9q/patches/camera/customize.sh
@@ -1,8 +1,6 @@
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlighting_gallery_360.so" 0 0 644 "u:object_r:system_lib_file:s0"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsuperresolution_raw.arcsoft.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libdualcam_refocus_image.so"
 
 
